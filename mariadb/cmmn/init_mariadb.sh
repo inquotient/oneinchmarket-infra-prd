ae3af5670a2b727f82d4127ec7349f18bbbc4eaa
Ahnjisoo <sfau2014@gmail.com>
2023-09-02 01:37:17 +0900

    oneinchmarket infra prd mariadb modifies

--- mariadb/cmmn/init_mariadb.sh
+++ mariadb/cmmn/init_mariadb.sh
@@ -9,7 +9,7 @@ then
     sudo chmod 777 ~/mariadb/cmmn/tde
     (echo -n "1;" ; openssl rand -hex 32 ) | sudo tee -a ~/mariadb/cmmn/tde/tde_master
     openssl rand -hex 128 > ~/mariadb/cmmn/tde/tde_master.key
-    openssl enc -aes-256-cbc -md sha1 -pass file:/home/ahnjisoo/mariadb/cmmn/tde/tde_master.key -in /home/ahnjisoo/mariadb/cmmn/tde/tde_master -out /home/ahnjisoo/mariadb/cmmn/tde/tde_master.enc
+    openssl enc -aes-256-cbc -md sha1 -pass file:/home/ubuntu/mariadb/cmmn/tde/tde_master.key -in /home/ubuntu/mariadb/cmmn/tde/tde_master -out /home/ubuntu/mariadb/cmmn/tde/tde_master.enc
     sudo kubectl apply -f mariadb-configmap.yaml -f mariadb-headless.yaml -f mariadb-statefulset.yaml -f mariadb-nodeport.yaml -n dev
     sudo kubectl -n dev port-forward svc/cmmn-mariadb-nodeport 10306:10306 > /dev/null 2>&1 &
 else
