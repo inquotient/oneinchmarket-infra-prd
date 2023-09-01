65423b6f4f5d45a9783740de8ac15d2e307289ca
inquotient <sfau2014@gmail.com>
2023-09-02 01:04:17 +0900

    oneinchmarket prd infra adds affinity and modifies storageClassName

--- zookeeper/zookeeper.sh
+++ zookeeper/zookeeper.sh
@@ -6,4 +6,4 @@
 
 # Replacing zookeeper-statefulset.yaml according to replica number
 replace=$(seq 1 $(yq '.spec.replicas' zookeeper-statefulset.yaml) | awk -v HOSTNAME="$(yq '.metadata.name' zookeeper-statefulset.yaml)" -v SERVICENAME="$(yq '.metadata.name' zookeeper-headless.yaml)" '{print "server." $1 "=" HOSTNAME "-" $1-1 "." SERVICENAME ".dev.svc.cluster.local:2888:3888;2181"}' | xargs echo) yq -i '.spec.template.spec.containers.[] | select(.name == "zookeeper") | .env.[] | select(.name == "ZOO_SERVERS") | .value=env(replace) | parent | parent | parent | parent | parent | parent | parent | parent' zookeeper-statefulset.yaml
-sudo kubectl create -f zookeeper-statefulset.yaml -f zookeeper-headless.yaml -n dev
+kubectl create -f zookeeper-statefulset.yaml -f zookeeper-headless.yaml -n dev
