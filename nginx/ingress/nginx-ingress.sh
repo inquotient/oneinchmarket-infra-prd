219dbcfa7be4284b280c514717b8ecd431920968
inquotient <sfau2014@gmail.com>
2023-09-10 20:43:28 +0900

    ingress name modified and argocd ingress added

--- nginx/ingress/nginx-ingress.sh
+++ nginx/ingress/nginx-ingress.sh
@@ -1,3 +1,4 @@
+sudo kubectl apply -f argocd-ingress.yaml -n argocd
 sudo kubectl apply -f gitlab-ingress.yaml -n dev
 sudo kubectl apply -f admin-ingress.yaml -n dev
 sudo kubectl apply -f kafka-ui-ingress.yaml -n dev
